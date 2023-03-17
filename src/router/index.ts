404a4cabeaa0e4b33bb33d99c1001dd86fddaa9a
Francisco Aloi <franad89@gmail.com>
2023-03-17 14:33:16 +0000

    store and router changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,27 +2,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AuthLayout from "@/layouts/Auth.vue";
 
-// TODO: implement guards to redirect if user not logged in
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: () => import(/* webpackChunkName: "Home" */ "@/views/Home.vue"),
-  },
-  {
-    path: "/sign-in",
-    name: "SignIn",
-    // props of the route
-    meta: { layout: AuthLayout },
-    component: () =>
-      import(/* webpackChunkName: "SignIn" */ "@/views/SignIn.vue"),
-  },
-];
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: () =>
+        import(/* webpackChunkName: "Home" */ "@/views/Home.vue"),
+    },
+    {
+      path: "/sign-in",
+      name: "Auth",
+      meta: { layout: AuthLayout },
+      component: () =>
+        import(/* webpackChunkName: "SignIn" */ "@/views/Auth.vue"),
+    },
+  ],
 });
 
 export default router;
--- src/store/app.ts
+++ src/store/app.ts
@@ -5,8 +5,12 @@ export const useAppStore = defineStore("app", {
   state: () => ({
     showDialogForm: false,
     images: new Array<Image>(),
+    user: null,
   }),
   actions: {
+    setUser(session: any) {
+      this.user = session;
+    },
     addImages(images: Image[]) {
       this.images.push(...images);
     },
