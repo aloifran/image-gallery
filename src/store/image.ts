b7fd569bb9af72fbad4e7a760943feb19b48d2cd
Francisco Aloi <franad89@gmail.com>
2023-05-19 21:44:58 +0000

    feat: created image store

--- src/store/image.ts
+++ src/store/image.ts
@@ -5,18 +5,24 @@ import { supabase } from "@/lib/supabase";
 // store for image management tasks
 
 interface ImageState {
-  showDialogForm: boolean;
-  showImageDialog: boolean;
+  showUploaderDialog: boolean;
+  showImageDialogPreview: boolean;
+  showImageDialogDelete: boolean;
   images: Image[];
+  image: null | Image;
   imgId: null | number;
+  prettyDate: string;
 }
 
 export const useImageStore = defineStore("image", {
   state: (): ImageState => ({
-    showDialogForm: false,
-    showImageDialog: false,
+    showUploaderDialog: false,
+    showImageDialogPreview: false,
+    showImageDialogDelete: false,
     images: new Array<Image>(),
     imgId: null,
+    image: null,
+    prettyDate: "",
   }),
 
   actions: {
@@ -30,13 +36,78 @@ export const useImageStore = defineStore("image", {
 
     openImageDialog(id: number) {
       this.imgId = id;
-      this.showImageDialog = true;
+      this.getImageData();
+      this.showImageDialogPreview = true;
     },
 
     refreshGallery() {
-      this.showImageDialog = false;
+      this.showImageDialogPreview = false;
+      this.showImageDialogDelete = false;
       this.images = [];
       this.getImagesBatch();
+    },
+
+    async getImageData() {
+      const { data: image, error: selectError } = await supabase
+        .from("images")
+        .select()
+        .eq("id", this.imgId)
+        .single();
+
+      if (selectError) {
+        throw selectError;
+      }
+
+      this.image = image;
+      this.prettyDate = new Date(image!.created_at).toLocaleDateString("de-DE");
+    },
+
+    resetImageData() {
+      // do this against a default object?
+      this.image = null;
+      this.imgId = null;
+      this.prettyDate = "";
+    },
+
+    // TODO: refactor edit image data logic
+    // async editimage() {
+    //   const { error: updateError } = await supabase
+    //     .from("images")
+    //     .update({
+    //       title: this.image!.title,
+    //       description: this.image!.description,
+    //     })
+    //     .eq("id", this.imgId);
+
+    //   if (updateError) {
+    //     throw updateError;
+    //   }
+
+    //   showEdit.value = false;
+    //   this.getImageData();
+
+    //   this.refreshGallery();
+    // },
+
+    async deleteImage() {
+      const filePath = this.image!.url.split("images/")[1].split("?")[0];
+
+      const { error: deleteStorageError } = await supabase.storage
+        .from("images")
+        .remove([filePath]);
+      if (deleteStorageError) {
+        throw deleteStorageError;
+      }
+
+      const { error: deleteDbError } = await supabase
+        .from("images")
+        .delete()
+        .eq("id", this.imgId);
+      if (deleteDbError) {
+        throw deleteDbError;
+      }
+
+      this.refreshGallery();
     },
 
     async getImagesBatch() {
